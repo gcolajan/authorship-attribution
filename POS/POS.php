3235dfc923dad708ed1c12716617cc0289c115b5
Colajanni Gautier <colag.contact@gmail.com>
2015-03-05 09:57:03 +0100

    Updating

--- POS/POS.php
+++ POS/POS.php
@@ -1,44 +1,77 @@
 #! /usr/bin/php
 <?php
+define('PATH', '../../Reuters50_50');
+define('TMP_FILE', 'analyze');
 
 if (count($argv) != 2)
-	exit("USAGE: "+$argv[0]+" <file_to_compare>");
+	exit("USAGE: ".$argv[0]." <file_to_compare>\n");
+	
+$file = $argv[1];
+
+$frequency = getCorpusFrequency(PATH.'/frequency');
+
+// Analyse du fichier soumis
+exec("cat $file | tree-tagger-english | cut -f 2 | grep [A-Z] > ".TMP_FILE);
+$analyse = getFrequency(TMP_FILE);
+unlink(TMP_FILE);
+
+// Comparaison des fréquences du fichier analysé avec le corpus
+$diff = array();
+foreach ($frequency as $author => $tags) {
+	$diff[$author] = 0.0;
+	foreach ($tags as $tag => $freq)
+	{
+		if (!isset($analyse[$tag]))
+			$analyse[$tag] = 0.0;
+			
+		$diff[$author] += abs($analyse[$tag] - $freq);
+	}
+}
+
+asort($diff);
+print_r($diff);
 
 
-function getFrequency() {
+function getCorpusFrequency($path) {
 	$frequency = array();
 
-	$path = 'Reuters50_50/frequency';
-	$dir = opendir($path) or die('Directory doenst exist'); // on ouvre le contenu du dossier courant
+	$dir = opendir($path) or die('Directory doenst exist');
 
 	$handle = opendir($path);
 	while($author = readdir($handle))
 	{
 		if($author != '.' && $author != '..')
 		{
-			$frequency[$author] = array();
-			$nbWords = 0;
-	
-			$file = fopen($path.'/'.$author, 'r');
-			if ($file)
-			{
-				while (!feof($file))
-				{
-					$word = fgets($file);
-					if (!isset($frequency[$author][$word]))
-						$frequency[$author][$word] = 0;
-					
-					$frequency[$author][$word]++;
-					$nbWords++;
-				}
-				fclose($file);
-			}
-			
-			foreach ($frequency[$author] as $tag => $occ)
-				$frequency[$author][$tag] = $occ/$nbWords;
+			$frequency[$author] = getFrequency($path.'/'.$author);
 		}
 	}
 	closedir($handle);
 	return $frequency;
+}
+
+
+function getFrequency($fileName) {
+	$freq = array();
+	$nbWords = 0;
+
+	$file = fopen($fileName, 'r');
+	if ($file)
+	{
+		while (!feof($file))
+		{
+			$word = trim(fgets($file));
+			if (!isset($freq[$word]))
+				$freq[$word] = 0;
+			
+			$freq[$word]++;
+			$nbWords++;
+		}
+		fclose($file);
+	}
+	
+	foreach ($freq as $tag => $occ)
+		$freq[$tag] = $occ/$nbWords;
+		
+	return $freq;
 }
 ?>
--- POS/POS_frequency.sh
+++ POS/POS_frequency.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd Reuters50_50/C50train/
+cd ../../Reuters50_50/C50train/
 
 for author in *; do
 	cat $author/*.txt | tree-tagger-english | cut -f 2 | grep [A-Z] > "../frequency/$author"
